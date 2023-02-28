3dc737b499c9863f220ac3238b765f9bca41685e
Keenan Urdiales <keenanurdiales@icloud.com>
2023-02-28 13:00:45 -0700

    setup chatengine.io and styled with css.

---
+++ chat-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {ChatEngine} from 'react-chat-engine'
+
+function App() {
+  return (
+ <ChatEngine
+ height="100vh"
+ projectID= "9574f69b-cc93-42ce-9d56-b7c65640b603"
+userName="keenanurdiales"
+userSecret="12345"
+ />
+  );
+}
+
+export default App;
